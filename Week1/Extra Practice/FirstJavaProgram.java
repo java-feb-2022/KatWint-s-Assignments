4ca7c41e2d09a5a710791fd8bc2d51b6e9df9cd5
Kathryn Wint <wintkathryn@gmail.com>
2022-02-10 18:34:03 -0800

    Retrying with Java 8 running

---
+++ Week1/Extra Practice/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {   
+        System.out.println('My name is Kat I am 32 years old My hometown is Sacramento, CA');
+    }
+}
