2cc9bdf2065272279348c72df44e307e68e115d7
Kathryn Wint <wintkathryn@gmail.com>
2022-02-13 18:41:37 -0800

    first commit

---
+++ Week1/Cafe Business Logic/CafeUtil.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CafeUtil {
+
+    public int getStreakGoal() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    
+    public double getOrderTotal(double[] prices) {
+        double sum =0
+        for (double total : items) {
+            sum+=total;
+        }
+        return sum
+    }
+}
---
+++ Week1/Cafe Business Logic/TestCafe.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class TestCafe {
+    public static void main(String[] args) {
+        CafeUtil cafeFunction = new CafeUtil();
+
+        cafeFunction.getStreakGoal();
+    
+        System.out.println("\n----- Streak Goal Test -----");
+        //System.out.printf("Purchases needed by week 10: %s \n\n", cafeFunction.getStreakGoal());
+  
+    }
+}
